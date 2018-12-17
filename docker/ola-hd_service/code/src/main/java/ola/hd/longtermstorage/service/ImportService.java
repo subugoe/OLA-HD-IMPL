6ad67884355b38c0fb592b24760aeaf414dda1fc
jpanzer <panzer@sub.uni-goettingen.de>
2018-12-17 10:27:18 +0100

    Cleanup
    Former-commit-id: e2e4c815d4a9c592cd399b15668c3606bc9c9042

--- docker/ola-hd_service/code/src/main/java/ola/hd/longtermstorage/service/ImportService.java
+++
@@ -1,8 +0,0 @@
-package ola.hd.longtermstorage.service;
-
-import java.io.File;
-import java.io.IOException;
-
-public interface ImportService {
-    void importZipFile(File file) throws IOException;
-}
