739084518de21ab0407328ea00581ad9d0d9fd2f
Triet Doan <triet.doan@gwdg.de>
2019-04-03 16:23:46 +0200

    Add @Repository annotation.

--- code/src/main/java/ola/hd/longtermstorage/repository/TrackingRepository.java
+++ code/src/main/java/ola/hd/longtermstorage/repository/TrackingRepository.java
@@ -2,7 +2,9 @@ package ola.hd.longtermstorage.repository;
 
 import ola.hd.longtermstorage.domain.TrackingInfo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TrackingRepository extends MongoRepository<TrackingInfo, String> {
 
     TrackingInfo findByPid(String pid);
--- code/src/main/java/ola/hd/longtermstorage/repository/UserRepository.java
+++ code/src/main/java/ola/hd/longtermstorage/repository/UserRepository.java
@@ -4,6 +4,7 @@ import ola.hd.longtermstorage.domain.User;
 import org.springframework.data.ldap.repository.LdapRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends LdapRepository<User> {
 
     User findByUsername(String username);
