495de83bb6a81706a2ed800acda7e1ff964b11c6
Triet Doan <triet.doan@gwdg.de>
2019-04-03 15:07:56 +0200

    Add LDAP authentication with an embedded LDAP server for testing purposes.

---
+++ code/src/main/java/ola/hd/longtermstorage/config/LdapSecurityConfig.java
@@ -0,0 +1,49 @@
+package ola.hd.longtermstorage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class LdapSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                .authorizeRequests()
+                    .antMatchers("/bag").authenticated()
+                    .anyRequest().permitAll()
+                    .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                    .userDnPatterns("uid={0},ou=people")
+                    .groupSearchBase("ou=groups")
+                    .contextSource()
+                        .ldif("classpath:test-server.ldif")
+                        .root("dc=springframework,dc=org")
+                        .port(8389)
+                    .and()
+                .passwordCompare()
+                    .passwordEncoder(passwordEncoder())
+                    .passwordAttribute("userPassword");
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
