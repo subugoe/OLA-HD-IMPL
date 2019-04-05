a9d193817a71fe6b7efe0a3a81ce2a87a0edee01
Triet Doan <triet.doan@gwdg.de>
2019-04-05 15:25:19 +0200

    Return proper message for invalid token.

--- code/src/main/java/ola/hd/longtermstorage/filter/JwtFilter.java
+++ code/src/main/java/ola/hd/longtermstorage/filter/JwtFilter.java
@@ -1,7 +1,14 @@
 package ola.hd.longtermstorage.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import ola.hd.longtermstorage.component.TokenProvider;
+import ola.hd.longtermstorage.domain.ResponseMessage;
 import ola.hd.longtermstorage.utils.SecurityConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -12,6 +19,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class JwtFilter extends GenericFilterBean {
@@ -46,12 +54,28 @@ public class JwtFilter extends GenericFilterBean {
             // Remove the authentication after the request to remain stateless
             SecurityContextHolder.clearContext();
 
-        } catch (Exception ex) {
+        } catch (BadCredentialsException ex) {
 
             // Make sure to clear the context in case something is wrong
             SecurityContextHolder.clearContext();
 
-            throw ex;
+            // Create a meaning full message
+            ResponseMessage responseMessage = new ResponseMessage(HttpStatus.UNAUTHORIZED, ex.getMessage(),
+                    httpServletRequest.getRequestURI());
+
+            // Turn it into JSON
+            ObjectMapper mapper = new ObjectMapper();
+            JavaTimeModule module = new JavaTimeModule();
+            mapper.registerModule(module);
+            mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+            String body = mapper.writeValueAsString(responseMessage);
+
+            // Send back to the users
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(body);
         }
     }
 
