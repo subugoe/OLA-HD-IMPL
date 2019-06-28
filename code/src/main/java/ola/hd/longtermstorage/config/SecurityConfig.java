53f7a574ec2d4342dda9b99bd8ffdd85f32dac6f
Triet Doan <triet.doan@gwdg.de>
2019-06-28 16:35:27 +0200

    Allow public access to the /docs endpoint.

--- code/src/main/java/ola/hd/longtermstorage/config/SecurityConfig.java
+++ code/src/main/java/ola/hd/longtermstorage/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/export",
                             "/login",
                             "/search",
+                            "/docs",
                             "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
