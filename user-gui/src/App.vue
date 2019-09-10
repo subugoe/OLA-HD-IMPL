6c604820bf84e4651485f651db27c16fbdff8e28
Triet Doan <triet.doan@gwdg.de>
2019-09-10 17:25:43 +0200

    Init commit. Navigation menu.

---
+++ user-gui/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-sm bg-light p-0">
+            <router-link tag="a" to="/" class="navbar-brand p-2">
+                <img src="./assets/logo-gwdg-full.png" alt="Brand logo">
+            </router-link>
+
+            <!-- Hamburger button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon">
+                    <i class="fa fa-bars"></i>
+                </span>
+            </button>
+
+            <!-- Navigation menu -->
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ml-auto">
+                    <router-link tag="li" class="nav-item px-2" active-class="active" to="/" exact>
+                        <a class="nav-link">Home</a>
+                    </router-link>
+                    <router-link tag="li" class="nav-item px-2" active-class="active" to="/about">
+                        <a class="nav-link">About</a>
+                    </router-link>
+                    <router-link tag="li" class="nav-item px-2" active-class="active" to="/contact">
+                        <a class="nav-link">Contact</a>
+                    </router-link>
+                    <router-link tag="li" class="nav-item px-2" active-class="active" to="/login">
+                        <a class="nav-link">Login</a>
+                    </router-link>
+                </ul>
+            </div>
+        </nav>
+        <router-view/>
+    </div>
+</template>
---
+++ user-gui/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+        <button class="btn btn-primary">Click me!</button>
+    </div>
+</template>
+
+<script lang="ts">
+    // import { Component, Vue } from 'vue-property-decorator';
+    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+    // @Component({
+    //     components: {
+    //         HelloWorld,
+    //     },
+    // })
+    // export default class Home extends Vue {}
+    export default {
+        components: {
+            HelloWorld: HelloWorld
+        },
+        created() {
+            console.log("On Home Create!");
+            console.log(process.env.BASE_URL);
+            console.log(process.env.NODE_ENV);
+            console.log(process.env);
+        }
+    }
+</script>
