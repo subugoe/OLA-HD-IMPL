31b1a1cc29f1ca89942fe8bdaa658352c860fc15
Triet Doan <triet.doan@gwdg.de>
2019-09-11 18:19:12 +0200

    Add footer.

---
+++ user-gui/src/components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+    <section id="footer">
+        <div class="container-fluid">
+            <div class="row py-3">
+                <div class="col-md-7 align-self-center text-center text-md-left">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item mx-0">
+                            <router-link to="/about">About</router-link>
+                            <span class="mx-2">&#183;</span>
+                        </li>
+                        <li class="list-inline-item mx-0">
+                            <router-link to="/contact">Contact</router-link>
+                            <span class="mx-2">&#183;</span>
+                        </li>
+                        <li class="list-inline-item mx-0">
+                            <router-link to="/terms-of-use">Terms of Use</router-link>
+                            <span class="mx-2">&#183;</span>
+                        </li>
+                        <li class="list-inline-item mx-0">
+                            <router-link to="/privacy">Privacy Policy</router-link>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-5 align-self-center text-center text-md-right">
+                    <div>&copy; GWDG 2019. All Rights Reserved.</div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style lang="scss" scoped>
+    #footer {
+        background-color: $secondary;
+        color: white;
+
+        ul li.list-inline-item a {
+            color: white;
+            text-decoration: none;
+        }
+    }
+</style>
--- user-gui/src/views/Home.vue
+++ user-gui/src/views/Home.vue
@@ -3,6 +3,7 @@
         <app-header></app-header>
         <app-showcase></app-showcase>
         <app-feature-detail></app-feature-detail>
+        <app-footer></app-footer>
     </div>
 </template>
 
@@ -10,12 +11,14 @@
     import Header from '../components/Header';
     import Showcase from '../components/Showcase';
     import FeatureDetail from '../components/FeatureDetail';
+    import Footer from '../components/Footer';
 
     export default {
         components: {
             appHeader: Header,
             appShowcase: Showcase,
-            appFeatureDetail: FeatureDetail
+            appFeatureDetail: FeatureDetail,
+            appFooter: Footer
         }
     }
 </script>
