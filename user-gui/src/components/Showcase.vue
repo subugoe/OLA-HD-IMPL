617fb44754d40752be2d083c27d16b73e350d601
Triet Doan <triet.doan@gwdg.de>
2019-09-11 16:22:23 +0200

    Add features showcase.

---
+++ user-gui/src/components/Showcase.vue
@@ -0,0 +1,68 @@
+<template>
+    <section id="feature" class="bg-light">
+        <div class="container">
+            <div class="row pt-3">
+
+                <!-- Feature -->
+                <div class="col-sm-12 col-md-4">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <i class="fas fa-cogs fa-3x mb-3"></i>
+                            <h5 class="card-title">PID Support</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Donec quam ante, blandit quis ornare id, auctor nec dolor.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Feature -->
+                <div class="col-sm-12 col-md-4">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <i class="fas fa-archive fa-3x mb-3"></i>
+                            <h5 class="card-title">Long Term Archive</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Donec quam ante, blandit quis ornare id, auctor nec dolor.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Feature -->
+                <div class="col-sm-12 col-md-4">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <i class="far fa-check-circle fa-3x mb-3"></i>
+                            <h5 class="card-title">Easy to Use</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Donec quam ante, blandit quis ornare id, auctor nec dolor.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Showcase"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .card {
+        background-color: transparent;
+        border: none;
+
+        .card-body i {
+            color: $primary;
+        }
+    }
+</style>
--- user-gui/src/views/Home.vue
+++ user-gui/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <app-header></app-header>
+        <app-showcase></app-showcase>
     </div>
 </template>
 
 <script>
     import Header from '../components/Header';
+    import Showcase from '../components/Showcase';
 
     export default {
         components: {
-            appHeader: Header
+            appHeader: Header,
+            appShowcase: Showcase
         }
     }
 </script>
