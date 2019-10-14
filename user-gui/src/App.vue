a5a01d9a2c99625bd0fc0d8fd00637bc23080820
Triet Doan <triet.doan@gwdg.de>
2019-10-14 14:36:19 +0200

    Restructure the component display.

--- user-gui/src/App.vue
+++ user-gui/src/App.vue
@@ -37,16 +37,20 @@
                 </ul>
             </div>
         </nav>
+
+        <app-header></app-header>
         <router-view/>
         <app-footer></app-footer>
     </div>
 </template>
 
 <script>
+    import Header from './components/Header';
     import Footer from './components/Footer';
 
     export default {
         components: {
+            appHeader: Header,
             appFooter: Footer
         }
     }
--- user-gui/src/views/Home.vue
+++ user-gui/src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
     <div>
-        <app-header></app-header>
         <app-showcase></app-showcase>
         <app-feature-detail></app-feature-detail>
     </div>
 </template>
 
 <script>
-    import Header from '../components/Header';
     import Showcase from '../components/Showcase';
     import FeatureDetail from '../components/FeatureDetail';
 
     export default {
         components: {
-            appHeader: Header,
             appShowcase: Showcase,
             appFeatureDetail: FeatureDetail
         }
