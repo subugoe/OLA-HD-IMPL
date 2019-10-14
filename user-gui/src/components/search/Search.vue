c07024e454ee6b2dbdf3cbeee332308ad0148fc5
Triet Doan <triet.doan@gwdg.de>
2019-10-14 15:42:16 +0200

    Add the search layout.

---
+++ user-gui/src/components/search/Search.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+
+        <!-- Search status -->
+        <div class="row pt-3">
+            <p class="mr-auto">15 results (20 ms)</p>
+            <p class="text-right">Showing 1 - 12</p>
+        </div>
+
+        <!-- Search result -->
+        <div class="row">
+            <div class="col-md-6 col-lg-4 mb-3"
+                 v-for="result in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]" :key="result">
+                <app-search-result></app-search-result>
+            </div>
+        </div>
+
+        <!-- Navigation -->
+        <div class="row mb-3">
+            <div class="col-12 text-center">
+                <button class="btn btn-primary mr-5">Previous</button>
+                <button class="btn btn-primary">Next</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SearchResult from './SearchResult'
+
+    export default {
+        components: {
+            appSearchResult: SearchResult
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ user-gui/src/components/search/SearchResult.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <i class="fas fa-download float-right"></i>
+            <h5>Title</h5>
+        </div>
+        <div class="card-body">
+            <table class="table table-borderless table-sm">
+                <tbody>
+                    <tr>
+                        <td>PID:</td>
+                        <td>1234</td>
+                    </tr>
+                    <tr>
+                        <td>Identifier:</td>
+                        <td>1234</td>
+                    </tr>
+                    <tr>
+                        <td>Date:</td>
+                        <td>14.10.2019</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SearchResult"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .card .card-header {
+        i {
+            color: $primary;
+            cursor: pointer;
+        }
+
+        h5 {
+            color: $primary;
+        }
+    }
+</style>
