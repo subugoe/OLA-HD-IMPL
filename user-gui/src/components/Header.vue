608ffbdcd128103833b50e8aaea65797cc1ed072
Triet Doan <triet.doan@gwdg.de>
2019-10-15 15:48:27 +0200

    Fill the search box with the value from the URL query.

--- user-gui/src/components/Header.vue
+++ user-gui/src/components/Header.vue
@@ -32,7 +32,7 @@
     export default {
         data() {
             return {
-                query: ''
+                query: this.$route.query['q']
             }
         },
         methods: {
