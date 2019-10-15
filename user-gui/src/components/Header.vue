676b3aee0a7abc2e9365ad505cfa437fae934a52
Triet Doan <triet.doan@gwdg.de>
2019-10-15 15:51:36 +0200

    Code refactor.

--- user-gui/src/components/Header.vue
+++ user-gui/src/components/Header.vue
@@ -20,7 +20,6 @@
                                 </button>
                             </div>
                         </div>
-
                     </form>
                 </div>
             </div>
--- user-gui/src/components/search/Search.vue
+++ user-gui/src/components/search/Search.vue
@@ -57,7 +57,7 @@
         },
         watch: {
             '$route': 'search'
-        },
+        }
     }
 </script>
 
