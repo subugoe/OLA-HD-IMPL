5b2c9f14c5454a75b9f4f2e8b62759103f4d8f83
Triet Doan <triet.doan@gwdg.de>
2019-10-16 11:28:37 +0200

    Re-indent.

--- user-gui/src/main.ts
+++ user-gui/src/main.ts
@@ -2,14 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
-import 'bootstrap'
-import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
 
-import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: (h) => h(App),
+    router,
+    render: (h) => h(App),
 }).$mount('#app');
