fab2648cbdb468f1e4d0036dbaaa68a108947221
Triet Doan <triet.doan@gwdg.de>
2019-10-21 12:04:08 +0200

    Add Swagger definition.

--- back-end/src/main/java/ola/hd/longtermstorage/controller/SearchController.java
+++ back-end/src/main/java/ola/hd/longtermstorage/controller/SearchController.java
@@ -1,5 +1,6 @@
 package ola.hd.longtermstorage.controller;
 
+import io.swagger.annotations.*;
 import ola.hd.longtermstorage.domain.SearchRequest;
 import ola.hd.longtermstorage.domain.SearchResults;
 import ola.hd.longtermstorage.service.ArchiveManagerService;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@Api(description = "This endpoint is used to search in the system.")
 @RestController
 public class SearchController {
 
@@ -27,8 +29,14 @@ public class SearchController {
         this.archiveManagerService = archiveManagerService;
     }
 
+    @ApiOperation(value = "Search on archive.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Search success", response = SearchResults.class)
+    })
     @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> search(@RequestParam(name = "q") String query, @RequestParam(defaultValue = "1000") int limit) throws IOException {
+    public ResponseEntity<?> search(@ApiParam(value = "The query used to search.", required = true)
+                                        @RequestParam(name = "q") String query,
+                                    @RequestParam(defaultValue = "1000") int limit) throws IOException {
 
         SearchRequest searchRequest = new SearchRequest(query, limit);
 
@@ -37,8 +45,15 @@ public class SearchController {
         return ResponseEntity.ok(results);
     }
 
+    @ApiOperation(value = "Search for an archive based on its internal ID.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Search success", response = String.class)
+    })
     @GetMapping(value = "/search-archive/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> searchArchive(@PathVariable String id, @RequestParam(defaultValue = "false") boolean withFile) throws IOException {
+    public ResponseEntity<?> searchArchive(@ApiParam(value = "Internal ID of the archive.", required = true)
+                                               @PathVariable String id,
+                                           @ApiParam(value = "An option to include all files in return.")
+                                           @RequestParam(defaultValue = "false") boolean withFile) throws IOException {
 
         String info = archiveManagerService.getArchiveInfo(id, withFile);
 
