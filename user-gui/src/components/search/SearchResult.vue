3a582c2cf4d8a000d262e285ff627e2b74a46717
Triet Doan <triet.doan@gwdg.de>
2019-11-07 13:50:31 +0100

    Display more information on the search results.

--- user-gui/src/components/search/SearchResult.vue
+++ user-gui/src/components/search/SearchResult.vue
@@ -9,23 +9,55 @@
         <div class="card-body">
             <table class="table table-borderless table-sm">
                 <tbody>
-                    <tr>
-                        <td class="w-25">Archive ID:</td>
-                        <td class="w-75">{{ item.id }}</td>
-                    </tr>
-                    <tr>
-                        <td class="w-25">Type:</td>
-                        <td class="w-75">{{ item.type }}</td>
-                    </tr>
-                    <tr v-if="item.name">
-                        <td class="w-25">Path to file:</td>
-                        <td class="w-75">{{ item.name }}</td>
-                    </tr>
-                    <!-- TODO: Display created / modified date and size -->
-<!--                    <tr>-->
-<!--                        <td>Date:</td>-->
-<!--                        <td>14.10.2019</td>-->
-<!--                    </tr>-->
+                    <template v-if="item.type === 'archive'">
+                        <tr>
+                            <td class="w-25">Archive ID:</td>
+                            <td class="w-75">{{ item.id }}</td>
+                        </tr>
+                        <tr>
+                            <td class="w-25">State:</td>
+                            <td class="w-75">{{ item.detail.state }}</td>
+                        </tr>
+                        <tr>
+                            <td class="w-25">Total file:</td>
+                            <td class="w-75">{{ item.detail.file_count }}</td>
+                        </tr>
+                        <tr>
+                            <td class="w-25">Created date:</td>
+                            <td class="w-75">{{ item.detail.created | formatDate }}</td>
+                        </tr>
+                        <tr>
+                            <td class="w-25">Last modified date:</td>
+                            <td class="w-75">{{ item.detail.modified | formatDate }}</td>
+                        </tr>
+                    </template>
+
+                    <template v-if="item.type === 'file'">
+                        <tr>
+                            <td class="w-25">File name:</td>
+                            <td class="w-75">{{ title }}</td>
+                        </tr>
+                        <tr>
+                            <td class="w-25">File type:</td>
+                            <td class="w-75">{{ item.detail.type }}</td>
+                        </tr>
+                        <tr>
+                            <td class="w-25">File size:</td>
+                            <td class="w-75">{{ item.detail.size }} KB</td>
+                        </tr>
+                        <tr>
+                            <td class="w-25">Path to file:</td>
+                            <td class="w-75">{{ item.name }}</td>
+                        </tr>
+                        <tr>
+                            <td class="w-25">Created date:</td>
+                            <td class="w-75">{{ item.detail.created | formatDate }}</td>
+                        </tr>
+                        <tr>
+                            <td class="w-25">Last modified date:</td>
+                            <td class="w-75">{{ item.detail.modified | formatDate }}</td>
+                        </tr>
+                    </template>
                 </tbody>
             </table>
         </div>
@@ -33,9 +65,18 @@
 </template>
 
 <script>
+    import moment from 'moment';
+
     export default {
         props: {
             item: Object
+        },
+        filters: {
+            formatDate(value) {
+                if (value) {
+                    return moment(String(value)).format('DD/MM/YYYY HH:mm');
+                }
+            }
         },
         computed: {
             title() {
