e8a80e3349e8acfdd2f6862f4d64e6f55446e590
Triet Doan <triet.doan@gwdg.de>
2020-04-01 20:33:43 +0200

    Remove TODO

--- admin-gui/src/axios-config.ts
+++ admin-gui/src/axios-config.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // TODO: Change the base URL to /api/ only
     baseURL: 'http://localhost/api',
     headers: {
         'Content-Type': 'application/json'
