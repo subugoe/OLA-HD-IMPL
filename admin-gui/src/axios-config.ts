933097015d89ca096f4462ff1bdf2c1285b69bb5
Triet Doan <triet.doan@gwdg.de>
2020-04-02 11:04:46 +0200

    Fix baseURL.

--- admin-gui/src/axios-config.ts
+++ admin-gui/src/axios-config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost/api',
+    baseURL: '/api/',
     headers: {
         'Content-Type': 'application/json'
     }
