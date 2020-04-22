252aaa4759b1642ae671ae0e18971949fea1a62b
Triet Doan <triet.doan@gwdg.de>
2020-04-22 16:01:41 +0200

    Return expired time of the token.

--- back-end/src/main/java/ola/hd/longtermstorage/domain/TokenResponse.java
+++ back-end/src/main/java/ola/hd/longtermstorage/domain/TokenResponse.java
@@ -1,15 +1,20 @@
 package ola.hd.longtermstorage.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import ola.hd.longtermstorage.utils.SecurityConstants;
+
+import java.util.Date;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TokenResponse {
 
     private String accessToken;
     private String tokenType = "Bearer";
+    private long expiredTime;
 
     public TokenResponse(String accessToken) {
         this.accessToken = accessToken;
+        this.expiredTime = new Date().getTime() + SecurityConstants.EXPIRATION_TIME;
     }
 
     public String getAccessToken() {
@@ -26,5 +31,9 @@ public class TokenResponse {
 
     public void setTokenType(String tokenType) {
         this.tokenType = tokenType;
+    }
+
+    public long getExpiredTime() {
+        return expiredTime;
     }
 }
