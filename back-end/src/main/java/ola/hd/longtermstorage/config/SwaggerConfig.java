f058800e886cabd3815ce9553af1b839f632fec8
Triet Doan <triet.doan@gwdg.de>
2020-06-25 22:47:14 +0200

    Fix conflict between Swagger and HATEOAS.

--- back-end/src/main/java/ola/hd/longtermstorage/config/SwaggerConfig.java
+++ back-end/src/main/java/ola/hd/longtermstorage/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package ola.hd.longtermstorage.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
@@ -39,6 +43,14 @@ public class SwaggerConfig {
                 .protocols(Set.of("http"))
                 .securitySchemes(buildSecuritySchemes())
                 .useDefaultResponseMessages(false);
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
     }
 
     private List<SecurityScheme> buildSecuritySchemes() {
