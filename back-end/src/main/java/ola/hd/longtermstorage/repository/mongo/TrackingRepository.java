db26b2c684a2e98693d8f6a764484bd390ba1e8e
Triet Doan <triet.doan@gwdg.de>
2020-06-27 13:29:34 +0200

    Remove unused methods.

--- back-end/src/main/java/ola/hd/longtermstorage/repository/mongo/TrackingRepository.java
+++ back-end/src/main/java/ola/hd/longtermstorage/repository/mongo/TrackingRepository.java
@@ -1,7 +1,6 @@
 package ola.hd.longtermstorage.repository.mongo;
 
 import ola.hd.longtermstorage.domain.TrackingInfo;
-import ola.hd.longtermstorage.domain.TrackingStatus;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +10,5 @@ import java.util.List;
 @Repository
 public interface TrackingRepository extends MongoRepository<TrackingInfo, String> {
 
-    TrackingInfo findByPid(String pid);
-    TrackingInfo findByOnlineIdOrOfflineId(String onlineId, String offlineId);
     List<TrackingInfo> findByUsername(String username, Pageable pageable);
 }
